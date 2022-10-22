414a68a35a4ebe8e98ae0ec291dd4cbf0fd16b6a
BGregV <greguar@ninodom.ru>
2022-10-22 10:25:50 +0300

    Второе задание

---
+++ DZ.2_Seminar2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите первое число:");
+double pervoeChislo = double.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите второе число:");
+double vtoroeChislo = double.Parse(Console.ReadLine() ?? "0");
+double kvadrat = Math.Pow(vtoroeChislo, 2);
+double kvadrat2 = Math.Pow(pervoeChislo, 2);
+if (pervoeChislo == kvadrat)
+{
+    Console.WriteLine("Первое число квадрат второго");
+}
+else if (vtoroeChislo == kvadrat2)
+{
+    Console.WriteLine("Второе число квадрат первого");
+}
+else
+{
+    Console.WriteLine("Первое число не квадрат второго");
+}
