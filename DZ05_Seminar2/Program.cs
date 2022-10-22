923385c74bc0fa99142ad20d9eec35c2d5bea70f
BGregV <greguar@ninodom.ru>
2022-10-22 10:40:14 +0300

    Пятое задание

---
+++ DZ05_Seminar2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите X1:");
+int x1 = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите Y1:");
+int y1 = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите Z1:");
+int z1 = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите X2:");
+int x2 = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите Y2:");
+int y2 = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите Z2:");
+int z2 = int.Parse(Console.ReadLine() ?? "0");
+
+double Rasstoianie(int x11, int y11, int z11, int x12, int y12, int z12)
+    {
+        return Math.Sqrt(Math.Pow(x12 - x11, 2) + Math.Pow(y12 - y11, 2)+Math.Pow(z12 - z11, 2));
+    }
+
+Console.Write("Расстояние между точками: {0}", Rasstoianie(x1,y1,z1,x2,y2,z2));
